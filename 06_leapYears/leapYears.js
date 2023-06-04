dfff1a2117ae9427b770a0234e0f70fa1b27d917
CatoTheYounger97 <82458228+CatoTheYounger97@users.noreply.github.com>
2023-06-05 00:34:39 +0100

    Finish exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) 
+{
+    let isLeap = false;
+
+    if (year % 4 === 0) {
+        isLeap = true;
+
+        if (year % 100 === 0) {
+            isLeap = false;
+        }
+        if (year % 400 === 0) {
+            isLeap = true;
+        }
+    }
+
+    return isLeap;
+
 
 };
 
