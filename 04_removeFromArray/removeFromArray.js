1c83e17db8d8fb8a26d94d51a5f67f2a98cd2525
CatoTheYounger97 <82458228+CatoTheYounger97@users.noreply.github.com>
2023-06-04 23:12:53 +0100

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) // can pass as many arguments as desired
+{
+    for (let i = 1; i < arguments.length; ++i)
+    {
+        const removeIndex = array.indexOf(arguments[i]);
+
+        if (removeIndex === -1) // returns -1 if array element isn't found.
+            continue;
+
+        array.splice(removeIndex, 1)
+    }
+
+    return array;
 
 };
 
