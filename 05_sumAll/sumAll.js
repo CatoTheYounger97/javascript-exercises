bc3fc13d9828a0aebc827afda14261c7e31a8c4e
CatoTheYounger97 <82458228+CatoTheYounger97@users.noreply.github.com>
2023-06-04 23:48:43 +0100

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) 
+{
+    if (a < 0 
+        || b < 0 
+        || typeof(a) != "number" 
+        || typeof(b) != "number") 
+        return "ERROR";
+
+    let bigNum = b;
+    let smolNum = a;
+    if ( a > b) {
+        bigNum = a;
+        smolNum = b;
+    }
+
+    let sumNum = 0;
+    for (let i = smolNum; i <= bigNum; ++i)
+    {
+        sumNum += i;        
+    }
+
+    return sumNum;
+
 
 };
 
