1a91354856273f21b727d7d46e61cde465693eb8
CatoTheYounger97 <82458228+CatoTheYounger97@users.noreply.github.com>
2023-06-05 01:45:55 +0100

    Finish exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) 
+{
+	let convert = (x - 32) * 5/9;
+	return (convert === 0 ? 0 : +convert.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) 
+{
+	let convert = x * 9/5 + 32;
+	return 	(convert === 0 ? 0 : +convert.toFixed(1));
 };
 
 // Do not edit below this line
