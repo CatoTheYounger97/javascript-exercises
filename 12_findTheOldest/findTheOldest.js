9e7812f6f9126140171e946b51a5a714f2738067
CatoTheYounger97 <82458228+CatoTheYounger97@users.noreply.github.com>
2023-06-12 19:05:42 +0100

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    const currentYear = new Date().getFullYear();
+
+    array.sort( (a, b) => {
+
+        const lifeA = ("yearOfDeath" in a) ? a.yearOfDeath : currentYear;
+        const lifeB = ("yearOfDeath" in b) ? b.yearOfDeath : currentYear; 
+
+         const aAge = lifeA - a.yearOfBirth;
+         const bAge = lifeB - b.yearOfBirth;
+
+        if (aAge < bAge)
+            return 1;
+        else if (aAge > bAge)
+            return -1;
+        else return 0;
+    });
+  
+    return array[0];
 };
 
 // Do not edit below this line
