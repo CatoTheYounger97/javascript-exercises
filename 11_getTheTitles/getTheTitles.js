581e85e32c74a075bc2052115ae55b924bb5c521
CatoTheYounger97 <82458228+CatoTheYounger97@users.noreply.github.com>
2023-06-12 16:28:47 +0100

    Finish exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = (array) => {
 
-};
+    newArray = array.map( (obj) => {
+      return obj.title;
+  
+    });
+  
+    return newArray;
+  
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
