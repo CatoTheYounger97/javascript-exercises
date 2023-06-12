77de47c97135a4facb9e84f920817f7ec69a4ba4
CatoTheYounger97 <82458228+CatoTheYounger97@users.noreply.github.com>
2023-06-12 13:43:31 +0100

    Finish exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;	
 };
 
-const sum = function() {
-	
+const sum = function(x) {
+  const sumArray = x.reduce( (acc, item) => {
+    return acc + item;
+  }, 0);	
+
+  return sumArray;
 };
 
-const multiply = function() {
+const multiply = function(x) {
+
+  const productArray = x.reduce( (accumulate, arg) => {
+    return accumulate * arg;
+  }, 1);
+
+  return productArray;
 
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+
+  return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+
+  if (x === 0) return 1;
+  
+  let fact = x;
+	for (let i = 1; i < x; ++i)
+  {
+    fact *= i;
+  }
+
+  return fact;
 };
 
 // Do not edit below this line
