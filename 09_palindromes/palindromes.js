69b81fa42024ce9e2ebf37b905168cd6d772908f
CatoTheYounger97 <82458228+CatoTheYounger97@users.noreply.github.com>
2023-06-12 14:44:12 +0100

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase();
+  
+    let stringArray = string.split("");
+    const charCodeA = "a".charCodeAt(0);
+    const charCodeZ = "z".charCodeAt(0);
+  
+    for (let i = 0; i < stringArray.length ; ++i)
+    {
+      const charCode = stringArray[i].charCodeAt(0);
+  
+      if (charCode < charCodeA || charCode > charCodeZ)
+        stringArray[i] = "";
+    }
+    
+    stringFiltered = stringArray.join("");
+    stringFilteredRev = stringArray.reverse().join("");
+  
+    return stringFiltered === stringFilteredRev;
 };
 
 // Do not edit below this line
